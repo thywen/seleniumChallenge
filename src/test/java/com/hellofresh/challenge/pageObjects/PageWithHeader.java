3773be6f30e85a3585ef7911a437527deb5f6138
Sven Kroell <sven@novoda.com>
2018-07-23 22:11:46 +0200

    add page with header

---
+++ src/test/java/com/hellofresh/challenge/pageObjects/PageWithHeader.java
@@ -0,0 +1,24 @@
+package com.hellofresh.challenge.pageObjects;
+
+import com.hellofresh.challenge.pageObjects.user.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public abstract class PageWithHeader extends BasePage {
+
+    @FindBy(linkText = "Woman")
+    private WebElement womanLink;
+
+    @FindBy(css = "a.login")
+    private WebElement loginLink;
+
+    protected PageWithHeader(WebDriver driver, String url) {
+        super(driver, url);
+    }
+
+    public LoginPage openLoginPage() {
+        loginLink.click();
+        return (LoginPage) andReturn(LoginPage.class, driver);
+    }
+}
